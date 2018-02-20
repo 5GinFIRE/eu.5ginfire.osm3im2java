f63c54c7ec97ec42181980a2e279f3e4112ea325
apalladinos <numgroup@150.140.240.94>
2018-02-20 13:55:11 +0200

    first commit

---
+++ src/main/java/eu/ginfire/osm3im2java/eu/ginfire/osm3im2java/App.java
@@ -0,0 +1,13 @@
+package eu.ginfire.osm3im2java.eu.ginfire.osm3im2java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
